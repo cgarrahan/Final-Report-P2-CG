2569a9e059e0359ffae34249de47a3c51ad8cc48
Claire <>
2023-12-09 12:46:15 -0600

    initial pretty setup

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
-st.text("Hello Kyle")
-st.button("Press Here")
+st.markdown("# Who are LinkedIn Users")
+st.markdown("### To determine who is likely a LinkedIn user, please enter data below")
+st.markdown("#### Income")
+#insert picture of breakdown here
+st.markdown("#### Gender (1 for Female, 0 for Male)")
+#st.button
+st.markdown("#### Education")
+#insert picture of breakdown here
+st.markdown("#### Parent (1 for Yes, 0 for No)")
+#st.button
+st.markdown("#### Married (1 for Yes, 0 for No)")
+#st.button
+st.markdown("#### Age")
+#st.button
